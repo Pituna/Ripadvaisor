0c6dca795d4c8870541bb443a88bbe1489a936b0
Pituna <jose_mmm@hotmail.es>
2026-05-12 20:40:35 +0100

    getter setter

--- advaisor/src/net/salesianos/restaurante/Restaurante.java
+++ advaisor/src/net/salesianos/restaurante/Restaurante.java
@@ -6,4 +6,43 @@ public class Restaurante {
     private String nombre;
     private String ciudad;
     private String tipo;
+
+    public Restaurante(int puntuacion,String nombre,String ciudad,String tipo){
+        this.puntuacion = puntuacion;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.tipo = tipo;
+    }
+
+    public int getPunt(){
+        return this.puntuacion;
+    }
+
+    public String getName(){
+        return this.nombre;
+    }
+
+    public String getCiudad(){
+        return this.ciudad;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public void setPunt(int punt){
+        this.puntuacion = punt;
+    }
+
+    public void setNombre(String name){
+        this.nombre = name;
+    }
+
+    public void setCiudad(String ciudad){
+        this.ciudad = ciudad;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
 }
